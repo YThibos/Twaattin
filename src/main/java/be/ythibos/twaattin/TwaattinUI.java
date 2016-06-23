5c345d0c1cc75429ae44b12f851c350804d89f47
Yannick Thibos <yannick.thibos@e2partners.be>
2016-06-23 16:37:44 +0200

    Cleanup

--- src/main/java/be/ythibos/twaattin/TwaattinUI.java
+++ src/main/java/be/ythibos/twaattin/TwaattinUI.java
@@ -1,26 +1,21 @@
 package be.ythibos.twaattin;
 
 import be.ythibos.twaattin.screens.LoginScreen;
-import be.ythibos.twaattin.screens.TimelineScreen;
-import com.google.gwt.user.client.Window;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.UI;
 
 import javax.servlet.annotation.WebServlet;
-
-import static com.vaadin.server.Sizeable.Unit.PIXELS;
 
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
  * (or tab) or some part of a html page where a Vaadin application is embedded.
- * <p>
+ * <p>d
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
  * overridden to add component to the user interface and initialize non-component functionality.
  */
@@ -33,8 +28,6 @@ public class TwaattinUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-
-
 
         setContent(new LoginScreen());
 
